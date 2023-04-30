7d3bad74fd6a53d6957ed96de01310d940d1cdb6
unknown <61201131+kevinCubas@users.noreply.github.com>
2023-04-30 01:32:37 -0300

    feat: page transitions

--- src/pages/FeedPage.tsx
+++ src/pages/FeedPage.tsx
@@ -1,10 +1,13 @@
 import { Feed } from "../components/Feed";
 import { ProtectedFeedRoute } from "../components/ProtectedRoute";
+import { motion } from "framer-motion";
 
 export default function FeedPage() {
   return (
     <ProtectedFeedRoute>
-      <Feed />
+      <motion.div initial={{ opacity: 0, y: -100 }} animate={{ opacity: 1, y: 0, transition: { ease: "easeIn", duration: 0.5 } }} exit={{ opacity: 0, y: -100 }}>
+        <Feed />
+      </motion.div>
     </ProtectedFeedRoute>
   )
 }
--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -2,12 +2,15 @@ import { Navigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { selectAuthUser } from "../redux/Features/authUserSlice";
 import { SignInForm } from "../components/SignInForm";
+import {motion} from "framer-motion";
 
 export default function SignIn() {
   const { isLoggedIn } = useSelector(selectAuthUser);
 
-  if(isLoggedIn) return <Navigate to="/" replace />
+  if (isLoggedIn) return <Navigate to="/" replace />
   return (
-    <SignInForm />
+    <motion.div initial={{ opacity: 0, x: -100 }} animate={{ opacity: 1, x: 0, transition: { ease: "easeIn" , duration: 0.5 }}} exit={{ opacity: 0 }}>
+      <SignInForm />
+    </motion.div>
   )
 }
